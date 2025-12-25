8ab9d6b69f32044e5f0b3edb803163d9f2c593b4
Abhay-k12 <abhaykanojia51@gmail.com>
2025-12-26 01:08:08 +0530

    Updation into default functions

--- journalApp/src/main/java/net/emgineeringdigest/journalApp/repository/UserRepository.java
+++ journalApp/src/main/java/net/emgineeringdigest/journalApp/repository/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
     User findByUserName(String userName);
+
+    void deleteByUserName(String userName);
     /*
         Why does this work without a method body?
         Answer: Spring Data MongoDB generates the method implementation at runtime using method name parsing (query derivation).
