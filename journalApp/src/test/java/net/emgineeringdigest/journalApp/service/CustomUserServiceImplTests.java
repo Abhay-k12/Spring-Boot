247becc2187d84ab6a96c56c2e8d188ab4d7308c
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-04 00:50:06 +0530

    Introduction to mokito to simulate the testing

---
+++ journalApp/src/test/java/net/emgineeringdigest/journalApp/service/CustomUserServiceImplTests.java
@@ -0,0 +1,45 @@
+package net.emgineeringdigest.journalApp.service;
+
+import net.emgineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+// @SpringBootTest - This will be used in case we are making use of @Autowired and @MockBeans
+public class CustomUserServiceImplTests {
+
+    @InjectMocks
+    private CustomUserServiceImpl userDetailsService;
+    /* Because I want to test loadUserByUsername(String) method, so call it I want the object of that class */
+
+    @Mock
+    private UserRepository userRepository;
+    /*  Since the loadUserByUsername(String) is calling userRepository, so we need bean of this type as well,
+        During runtime the @InjectMocks private CustomUserServiceImpl userDetailsService; will be automatically initialized
+        But the @Mock private UserRepository userRepository; will remain as null since things are not autowired here,
+        That's why we need a function to initialize this mocks.*/
+
+    @BeforeEach
+    void setupMocks() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void loadUserByUsernameTest() {
+        when(userRepository.findByUserName(ArgumentMatchers.anyString()))
+                .thenReturn(net.emgineeringdigest.journalApp.entity.User.builder().userName("Abhay").password("abhay").roles(new ArrayList<>()).build());
+        UserDetails userDetails = userDetailsService.loadUserByUsername("Abhay");
+        assertNotNull(userDetails);
+    }
+}
