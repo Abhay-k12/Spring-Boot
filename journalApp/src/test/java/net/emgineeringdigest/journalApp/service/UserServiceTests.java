f49b62b4adc9c3c4647d4d6aeb91b042194f9dbe
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-04 00:50:42 +0530

    Spring Test

--- journalApp/src/test/java/net/emgineeringdigest/journalApp/service/UserServiceTests.java
+++ journalApp/src/test/java/net/emgineeringdigest/journalApp/service/UserServiceTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest   //So that Spring will create component and beans (which was previously created by @SpringBootApplication annotation);
