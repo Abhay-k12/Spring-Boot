cc68c4826bea8292b31012a069e9a090da749856
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-08 23:14:45 +0530

    Studied Basics of logging

--- journalApp/src/main/java/net/emgineeringdigest/journalApp/JournalApplication.java
+++ journalApp/src/main/java/net/emgineeringdigest/journalApp/JournalApplication.java
@@ -9,19 +9,18 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class JournalApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
 	}
-
-    @Bean
-    /*
-        Spring during the time of the implementation of the transaction will look for the method which will return
-        the bean or instance of PlatformTransactionManager;
-     */
-    public PlatformTransactionManager abhay12(MongoDatabaseFactory dbFactory) {
-        return new MongoTransactionManager(dbFactory);
-    }
 }
+
+
+/*
+    Logging:
+    2026-01-08T23:05:05.583+05:30  INFO 1180 --- [           main] n.e.journalApp.JournalApplication        : Starting JournalApplication using Java 23.0.2 with PID 1180
+        ^                           ^     ^                             ^                                             ^
+        |                           |     |                             |                                             |
+    (Time stamp)          (Log Severity) (Process Id)           (Thread Identifier)                                 (Log)
+ */
