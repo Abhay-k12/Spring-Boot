1b1c83e41b6ef1351cc2d22d86ca0b34861c54e6
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-14 22:17:52 +0530

    Addition of external api, but somehow not responding

--- journalApp/src/main/java/net/engineeringdigest/journalApp/apiResponse/WeatherResponse.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/apiResponse/WeatherResponse.java
@@ -1,4 +1,40 @@
 package net.engineeringdigest.journalApp.apiResponse;
 
-public class WeatherResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WeatherResponse{
+    private Current current;
 }
+
+class Current{
+    @JsonProperty("observation_time")
+    private String observationTime;
+    private int temperature;
+
+    @JsonProperty("weather_code")
+    private int weatherCode;
+
+    @JsonProperty("wind_speed")
+    private int windSpeed;
+
+    @JsonProperty("wind_degree")
+    private int windDegree;
+
+    @JsonProperty("wind_dir")
+    private String windDir;
+
+    private int pressure;
+    private int precip;
+    private int humidity;
+    private int visibility;
+}
+
+
+
+
+
+
