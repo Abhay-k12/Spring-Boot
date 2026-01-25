16ceb0bcb4f18afa5a31c02a3920ccf428ac9b79
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-26 00:37:38 +0530

    Added response containder class to map acheive deserialisation

--- journalApp/src/main/java/net/engineeringdigest/journalApp/apiResponse/WeatherResponse.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/apiResponse/WeatherResponse.java
@@ -1,6 +1,7 @@
 package net.engineeringdigest.journalApp.apiResponse;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.websocket.server.ServerEndpoint;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,32 +9,34 @@ import lombok.Setter;
 @Setter
 public class WeatherResponse{
     private Current current;
+
+    @Getter
+    @Setter
+    public class Current{
+        @JsonProperty("observation_time")
+        private String observationTime;
+
+        private int temperature;
+
+        @JsonProperty("weather_code")
+        private int weatherCode;
+
+        @JsonProperty("wind_speed")
+        private int windSpeed;
+
+        @JsonProperty("wind_degree")
+        private int windDegree;
+
+        @JsonProperty("wind_dir")
+        private String windDir;
+
+        private int pressure;
+        private int precip;
+        private int humidity;
+        private int visibility;
+    }
 }
 
-@Getter
-class Current{
-    @JsonProperty("observation_time")
-    private String observationTime;
-
-    private int temperature;
-
-    @JsonProperty("weather_code")
-    private int weatherCode;
-
-    @JsonProperty("wind_speed")
-    private int windSpeed;
-
-    @JsonProperty("wind_degree")
-    private int windDegree;
-
-    @JsonProperty("wind_dir")
-    private String windDir;
-
-    private int pressure;
-    private int precip;
-    private int humidity;
-    private int visibility;
-}
 
 
 
