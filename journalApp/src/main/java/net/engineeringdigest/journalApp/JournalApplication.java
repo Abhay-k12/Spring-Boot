cf0609d8504055d8b8bbf2c55d326b5565cf6c47
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-26 00:40:02 +0530

    Added RestTemplate bean

--- journalApp/src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -17,12 +17,3 @@ public class JournalApplication {
         return new RestTemplate();
     }
 }
-
-
-/*
-    Logging:
-    2026-01-08T23:05:05.583+05:30  INFO 1180 --- [           main] n.e.journalApp.JournalApplication        : Starting JournalApplication using Java 23.0.2 with PID 1180
-        ^                           ^     ^                             ^                                             ^
-        |                           |     |                             |                                             |
-    (Time stamp)          (Log Severity) (Process Id)           (Thread Identifier)                                 (Log)
- */
