46e42afc59f2c23362aab04f71b6834152e4a4cd
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-27 22:39:24 +0530

    Addition of post API calling mechanism

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -1,10 +1,9 @@
 package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.apiResponse.WeatherResponse;
+import net.engineeringdigest.journalApp.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,5 +24,25 @@ public class WeatherService {
         HttpStatusCode res = response.getStatusCode();
 
         return response.getBody();
+    }
+
+    /* METHOD TO SHOW THE USE-CASE OF POST API CALLING, ONLY FOR EXAMPLE*/
+    public WeatherResponse getWeatherPostCallExample() {
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Key_attribute", "Value_attribute");
+
+        User user = User.builder().userName("Abhay").password("Anshika12@").build();
+        HttpEntity<User> httpEntity = new HttpEntity<>(user,httpHeaders);
+
+        ResponseEntity<WeatherResponse> response= restTemplate.exchange(API,HttpMethod.POST,httpEntity,WeatherResponse.class);
+
+        return response.getBody();
+
+        /*
+            "****** EVEN WE CAN USE THE STRING TO BUILD THE httpEntity ******"
+            String userWithString  = " provide the json format here";
+            HttpEntity<String> httpEntityWithString = new HttpEntity<>(userWithString);
+        */
     }
 }
