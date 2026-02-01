03a101571b871df90cfe8ea6ecd0a249bb7f6556
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-01 20:56:43 +0530

    Addition of entity class for Configuration collection

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/ConfigJournalAppEntity.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/ConfigJournalAppEntity.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 @Document("config_journal_app")
-public class ConfigJournalApp {
+public class ConfigJournalAppEntity {
+
     private String key;
     private String value;
+
 }
