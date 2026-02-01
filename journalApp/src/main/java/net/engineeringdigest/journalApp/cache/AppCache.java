478f00e1d5cf6197ecd699b851aeb18b6be00cf1
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-01 20:57:36 +0530

    Used @PostConstruct annotation to load all configs into map (cache)

--- journalApp/src/main/java/net/engineeringdigest/journalApp/cache/AppCache.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/cache/AppCache.java
@@ -1,4 +1,31 @@
 package net.engineeringdigest.journalApp.cache;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import net.engineeringdigest.journalApp.entity.ConfigJournalAppEntity;
+import net.engineeringdigest.journalApp.repository.ConfigJournalAppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@Data
 public class AppCache {
+
+    @Autowired
+    private ConfigJournalAppRepository configJournalAppRepository;
+
+    public Map<String,String> APP_CACHE = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        List<ConfigJournalAppEntity> all= configJournalAppRepository.findAll();
+
+        for(ConfigJournalAppEntity entry: all) {
+            APP_CACHE.put(entry.getKey(), entry.getValue());
+        }
+    }
 }
