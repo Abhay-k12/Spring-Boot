7d5c9265a7b68eb583ce94ff22de2556fb3c7ce1
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-03 09:04:08 +0530

    Added test for mongoTemplate usage

--- journalApp/src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImpTest.java
+++ journalApp/src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImpTest.java
@@ -1,4 +1,23 @@
 package net.engineeringdigest.journalApp.repository;
 
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Disabled
+@SpringBootTest
 public class UserRepositoryImpTest {
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Test
+    void getUserForSentimentAnalysisTestTest() {
+        assertEquals(1, userRepository.getUsersForSentimentAnalysis().size());
+    }
 }
