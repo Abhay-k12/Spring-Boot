9f6823a875acbe396a95f4c8a7dd88968ad8a7b0
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-03 09:02:58 +0530

    Added new feilds for user sentiment analysis

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -30,6 +30,10 @@ public class User {
     @NonNull
     private String password;
 
+    private String email;
+
+    private boolean sentimentAnalysis;
+
     @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
     /*
