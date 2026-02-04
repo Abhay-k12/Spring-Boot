1a11cbe56d0d56342ed88bb5cd3c859f81c0d5d7
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-04 08:44:36 +0530

    Successfully Implemented Email Functionality with Unit Testing

--- journalApp/src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTest.java
+++ journalApp/src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTest.java
@@ -1,4 +1,44 @@
 package net.engineeringdigest.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.context.aot.DisabledInAotMode;
+
+@Disabled
+@SpringBootTest
+@Slf4j
 public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    @ParameterizedTest
+    @CsvSource({
+            "abcd@gmail.com, Sample Message, Congratulations!! Hii, hoe are you doing?"
+    })
+    public void testMail(String to, String subject, String body) {
+        try {
+            emailService.sendEmail(to,subject,body);
+        }
+        catch(Exception e) {
+            System.out.println("Exception:" + e);
+        }
+    }
+
+    @Test
+    public void testMail() {
+        try {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            emailService.sendEmail("abcd@gmail.com","Leaning SpringBoot", "Learn SpringBoot and make Projects");
+        }
+        catch(Exception e) {
+            log.error("Exception :", e);
+        }
+    }
 }
