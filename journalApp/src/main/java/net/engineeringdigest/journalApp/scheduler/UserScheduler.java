57c6082b886ef909baabb14ec9b55cb1622096e3
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-04 09:12:54 +0530

    Implemented module to analyse users sentiment through journal entries

--- journalApp/src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
@@ -1,4 +1,38 @@
 package net.engineeringdigest.journalApp.scheduler;
 
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepositoryImpl;
+import net.engineeringdigest.journalApp.service.EmailService;
+import net.engineeringdigest.journalApp.service.SentimentAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+    public void fetchUsersForSentimentAnalysisAndMailSending() {
+        List<User> users = userRepository.getUsersForSentimentAnalysis();
+
+        for(User user: users) {
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream().filter(x->x.getDate().isAfter(LocalDateTime.now().minusDays(7))).map(JournalEntry::getContent).toList();
+            String entries = String.join(" ",filteredEntries);
+            String sentiment = sentimentAnalysisService.getSentimentAnalysis(entries);
+            emailService.sendEmail(user.getEmail(), "Sentiment Analysis for last 7 days.", sentiment);
+        }
+    }
 }
