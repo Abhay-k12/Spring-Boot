36a2d1212a5ec2329d13cc150f9e410f3ad4eb39
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-05 20:57:42 +0530

    Modified unit testing method of repositoty layer

--- journalApp/src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImpTest.java
+++ journalApp/src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImpTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Disabled
 @SpringBootTest
