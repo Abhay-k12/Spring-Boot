14c344a28649bd5439ed36a55e33edc66d9fadee
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-06 21:18:48 +0530

    Temporarily disabling unit test module

--- journalApp/src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
+++ journalApp/src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
@@ -3,7 +3,6 @@ package net.engineeringdigest.journalApp;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @Disabled
 @SpringBootTest
