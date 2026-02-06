d1aad65a0f5474d5a1bd5c107efb1f0141087dfc
Abhay-k12 <abhaykanojia51@gmail.com>
2026-02-06 21:17:39 +0530

    Updated testing mechanism

--- journalApp/src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
+++ journalApp/src/test/java/net/engineeringdigest/journalApp/service/UserServiceTests.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
